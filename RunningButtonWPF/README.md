573c6afbe02afdca9ed6252b9ddd74ead0f0ff39
Hayk Harutyunyan <hayk.haru@gmail..com>
2017-01-25 23:19:40 +0400

    Event Handler, Running Button

---
+++ RunningButtonWPF/README.md
@@ -0,0 +1,28 @@
+﻿# Running button
+###The project demonstrates the usage of Event Handler
+
+Some code is below:
+```C#
+private void button_MouseEnter(object sender, MouseEventArgs e)
+{
+    button.MouseEnter += button_rndMove;
+}
+        
+private void button_rndMove(object sender, MouseEventArgs e)
+{
+    Random rnd = new Random();
+
+    var width = rnd.Next(0, (int)Grid.Width - (int)button.Width);
+
+    var height = rnd.Next(0, (int)Grid.Height - (int)button.Height);
+
+    button.Margin = new Thickness(width, height, 0, 0);
+}
+```
+
+The result is below: <br\>
+![Translit Arm](/buttonGif.gif)
+
+
+
+> This project written by C#6.0 for .NET Framework 4.6.2 version on Visual Studio 2015 Comunity Edition
---
+++ RunningButtonWPF/RunningButtonWPF/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RunningButtonWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            Grid.Height = MyMainWindow.Height * 0.9; //(90%)
+            Grid.Width = MyMainWindow.Width * 0.9; //(90%)
+
+        }
+
+        private void button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            button.MouseEnter += button_rndMove;
+        }
+        
+        private void button_rndMove(object sender, MouseEventArgs e)
+        {
+            Random rnd = new Random();
+
+            var width = rnd.Next(0, (int)Grid.Width - (int)button.Width);
+
+            var height = rnd.Next(0, (int)Grid.Height - (int)button.Height);
+
+            button.Margin = new Thickness(width, height, 0, 0);
+        }
+    }
+}
