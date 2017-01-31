c3194c92dce0266409537b8fbf089f09f58871da
Hayk Harutyunyan <hayk.haru@gmail.com>
2017-01-31 23:04:36 +0400

    Web Content Downloader First Step

---
+++ DownloadFromWeb/DownloadFromWeb/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadFromWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectToWeb connect2Web = new ConnectToWeb();
+            //connect2Web.SaveInfo("http://google.com", "google.html");
+            connect2Web.SaveInfo("http://mic.am", "mic.html");
+        }
+    }
+}
---
+++ DownloadFromWeb/README.md
@@ -0,0 +1,7 @@
+<H1>Short review of Web Content Downloader</H1> 
+
+
+```C#
+```
+
+> This project written for .NET Framework 4.5.2 version, C# 6
