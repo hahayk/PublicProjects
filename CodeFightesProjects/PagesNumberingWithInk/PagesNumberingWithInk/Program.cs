3175a03d6d9123ccc7ee7d2f5694fff03f8a6791
Hayk Harutyunyan <hayk.haru@gmail.com>
2017-05-21 21:55:46 +0400

    Done

--- CodeFightesProjects/PagesNumberingWithInk/PagesNumberingWithInk/Program.cs
+++ CodeFightesProjects/PagesNumberingWithInk/PagesNumberingWithInk/Program.cs
@@ -10,14 +10,32 @@ namespace PagesNumberingWithInk
     {
         static void Main(string[] args)
         {
-            pagesNumberingWithInk(1, 5);
+            pagesNumberingWithInk(21, 5);
         }
 
         public static int pagesNumberingWithInk(int current, int numberOfDigits)
         {
+            int currentLastDigit  = current;
+            
+            int curNumberInks = Convert.ToInt32(Math.Floor(Math.Log10(current))) + 1;
+            numberOfDigits -= curNumberInks;
+            
+            while (numberOfDigits > 0)
+            {
+                curNumberInks = Convert.ToInt32(Math.Floor(Math.Log10(current++))) + 1;
+                numberOfDigits -= curNumberInks;
+                if (numberOfDigits >= 0)
+                {
+                    currentLastDigit = current;
+                }
+                else
+                {
+                    break;
+                }
 
-            return 0;
+            }
+            
+            return currentLastDigit;
         }
-
     }
 }
